babbdc566334d7c6eb6646fc3b2d1a003fcd1080
Leonardo Cunha <leo.francasp97@gmail.com>
2023-09-05 10:54:21 -0300

    AULADIA05-09

--- 2-semestre/introducao-javascript/3 - Exercícios Estrutura de repetição/lista3/lista3.js
+++ 2-semestre/introducao-javascript/3 - Exercícios Estrutura de repetição/lista3/lista3.js
@@ -30,14 +30,33 @@ function exe1 () {
 function exe2 () {
     let valingresso = 5
     let qtd = 120
-    let despesa = 200
+    const despesa = 200
+    let corpo = ``
     while (valingresso >= 1) {
         let lucro = qtd * valingresso - despesa
-        console.log(`Valor do ingresso: R$${valingresso}`)
-        console.log(`Quantidade de ingresso: ${qtd} unidades`)
-        console.log(`Valor do lucro: ${lucro} reais`)
-        
+        // console.log(`Preço: R$${valingresso.toFixed(2)} Quantidade: ${qtd.toFixed(2)} Lucro: R$${lucro.toFixed(2)}`)
+        corpo = corpo + `
+        <tr> 
+            <td>${valingresso.toFixed(2)}</td> 
+            <td>${qtd}</td> 
+            <td>${despesa}</td> 
+            <td>${lucro.toFixed(2)}</td> 
+        </tr>`
         valingresso = valingresso - 0.5
         qtd = qtd + 26 
     }
+    document.getElementById('corpo').innerHTML = corpo
+}
+
+function exe5 () {
+    let resposta = ``
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            let multiplicacao = i * j
+            resposta = resposta + `${i} x ${j} = ${multiplicacao}<br>`
+            // FAZER O EXERCÍCIO UTILIZANDO TABELA EM CASA!!!
+        }
+        resposta = resposta + `<br>`
+    }
+    document.getElementById('resultado').innerHTML = resposta
 }
