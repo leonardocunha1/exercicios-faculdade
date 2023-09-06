e145339f45ed46b35e392e882fdbf1ed40340594
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2023-09-05 21:39:34 -0300

    exercicios-aula-05-09

--- 2-semestre/introducao-javascript/3 - Exercícios Estrutura de repetição/lista3/lista3.js
+++ 2-semestre/introducao-javascript/3 - Exercícios Estrutura de repetição/lista3/lista3.js
@@ -1,4 +1,4 @@
-function exe1 () {
+function exe1() {
     for (let i = 0; i<4; i++) {
         let a = Number(prompt("Digite o valor de A"))
         let b = Number(prompt("Digite o valor de B"))
@@ -26,8 +26,7 @@ function exe1 () {
         console.log(`Ordem decrescente ${d} ${c} ${b} ${a}`)
     }
 }
-
-function exe2 () {
+function exe2() {
     let valingresso = 5
     let qtd = 120
     const despesa = 200
@@ -47,16 +46,236 @@ function exe2 () {
     }
     document.getElementById('corpo').innerHTML = corpo
 }
-
-function exe5 () {
+function exe3() {
+    let acm1 = 0
+    let acm2 = 0
+    let acm3 = 0
+    let acm4 = 0
+    let acm5 = 0
+    for (let i = 1; i <= 8; i++) {
+        let idade = Number(prompt("Digite sua idade"))
+        if (idade <= 0) {
+            alert(`Valor inválido`)
+        }
+        else {
+            if (idade <= 15) {
+                acm1 = acm1 + 1
+            }
+            else if (idade > 15 && idade <= 30) {
+                acm2 = acm2 + 1 
+            }
+            else if (idade > 30 && idade >= 45) {
+                acm3 = acm3 + 1
+            }
+            else if (idade > 45 && idade <=60) {
+                acm4 = acm4 + 1
+            }
+            else {
+                acm5 = acm5 + 1
+            }
+        }
+    }
+    document.getElementById('resultado').innerHTML = `
+    Há ${acm1} pessoas na 1° faixa etária<br>
+    Há ${acm2} pessoas na 2° faixa etária<br>
+    Há ${acm3} pessoas na 3° faixa etária<br>
+    Há ${acm4} pessoas na 4° faixa etária<br>
+    Há ${acm5} pessoas na 5° faixa etária<br>
+    Há ${(acm1/8)*100}% de pessoas na 1° faixa etária<br>
+    Há ${(acm5/8)*100}% de pessoas na 5° faixa etária`
+}
+function exe4() {
+    let mensagem = ``
+    let numero = parseFloat(document.getElementById("num").value)
+    for (let i=1; i<=10;i++) {
+        let multiplicacao = numero * i
+        mensagem = mensagem + `${numero} x ${i} = ${multiplicacao}<br>`
+    }
+    document.getElementById("resultado").innerHTML = mensagem
+}
+function exe5() {
     let resposta = ``
     for (let i = 1; i <= 10; i++) {
+        resposta = resposta + `Tabuada do ${i}<br>`
         for (let j = 1; j <= 10; j++) {
             let multiplicacao = i * j
-            resposta = resposta + `${i} x ${j} = ${multiplicacao}<br>`
-            // FAZER O EXERCÍCIO UTILIZANDO TABELA EM CASA!!!
+            resposta = resposta + `
+            <tr>
+                <td>${i} x ${j}</td>
+                <td>${multiplicacao}</td>
+            </tr>`
+            // resposta = resposta + `${i} x ${j} = ${multiplicacao}<br>`
         }
         resposta = resposta + `<br>`
     }
     document.getElementById('resultado').innerHTML = resposta
+}
+function exe6() {
+    // Inicializa as variáveis para armazenar os totais
+    let totalavista = 0
+    let totalaprazo = 0
+    let totalcompras = 0
+
+    // Loop para receber as transações
+    for (let i = 1; i <= 15; i++) {
+        let codigo = prompt("Digite o código da transação (V para à vista, P para a prazo) da transação " + i + ":")
+        let valor = Math.random() * 10000 + 1 //Sorteando os valores de 1 até 10 mil <- facilita muito o exercício
+
+        if (codigo == 'V' || codigo == 'v') {
+            totalavista = totalavista + valor
+        } 
+        else if (codigo == 'P' || codigo == 'p') {
+            totalaprazo = totalaprazo + valor
+        }
+
+        totalcompras = totalcompras + valor
+    }
+
+    // Calcula o valor da primeira prestação a prazo (em 3 vezes)
+    let primeiraprestacao = totalaprazo / 3
+
+    // Exibe os resultados
+    let resultadoex = `Valor total das compras à vista: R$ ${totalavista.toFixed(2)}<br>`
+    resultadoex = resultadoex + `Valor total das compras a prazo: R$ ${totalaprazo.toFixed(2)}<br>`
+    resultadoex = resultadoex + `Valor total das compras efetuadas: R$ ${totalcompras.toFixed(2)}<br>`
+    resultadoex = resultadoex + `Valor da primeira prestação das compras a prazo: R$ ${primeiraprestacao.toFixed(2)}`
+    document.getElementById('resultado').innerHTML = resultadoex
+}
+function exe7() {
+    let contidade = 0
+    let cont2 = 0
+    let acm = 0
+    let contpeso = 0
+    let mensagem = ``
+    for (let i=1;i<=5;i++) {
+        let altura = Number(prompt("Digite a altura da pessoa " + i + ":"))
+        let idade = Math.random() * 100 + 1
+        let peso = Math.random() * 200 + 1
+
+        if (idade > 50) {
+            contidade = contidade + 1
+        }
+        if (idade > 10 && idade < 20) {
+            acm = acm + altura
+            cont2 = cont2 + 1
+        }
+        if (peso < 40) {
+            contpeso = contpeso + 1
+        }
+    }
+    // Mensagem da quantidade de pessoas com idade superior a 50 anos
+    if (contidade != 0) {
+        mensagem = mensagem + `A quantidade de pessoas com idade superior a 50 anos é: ${contidade}<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas acima de 50 anos<br>`
+    }
+
+    // Cálculo da média e mensagem das alturas das pessoas com idade entre 10 e 20 anos
+    if (cont2 != 0) {
+        let media = acm/cont2
+        mensagem = mensagem + `A média das alturas das pessoas com idade entre 10 e 20 anos é: ${media}<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas entre 10 e 20 anos<br>`
+    }
+
+    // Cálculo porcentagem de pessoas com peso inferior a 40kg 
+    if (contpeso != 0) {
+        mensagem = mensagem + `A porcentagem de pessoas abaixo de 40kg é ${(contpeso/5)*100}%`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas abaixo de 40kg`
+    }
+    document.getElementById('resultado').innerHTML = mensagem
+}
+function exe8() {
+    let contpessoa = 0
+    let acmidade = 0
+    let contidade = 0
+    let conteye = 0
+    let cont1 = 0
+    let mensagem = ``
+    for (let i=1;i<=6;i++){
+        //Entrada de dados
+        let idade = Math.random() * 100 + 1
+        let peso = Math.random() * 90 + 1
+        let altura = Number(prompt("Digite a altura da pessoa " + i + ":"))
+        let coloreye = prompt("A - azul\nP - preto\nV - verde\nC - castanho\nDigite a cor dos olhos da pessoa " + i + ":")
+        let colorhair = prompt("L - louro\nP - preto\nR - ruivo\nC - castanho\nDigite a cor dos olhos da pessoa " + i + ":")
+        //Cálculos e operações
+        if (idade > 50 && peso < 60) {
+            contpessoa = contpessoa + 1
+        }
+        if (altura < 1.5) {
+            acmidade = acmidade + idade
+            contidade = contidade + 1
+        }
+        if (coloreye == 'A' || coloreye == 'a') {
+            conteye = conteye + 1
+        }
+        if ((colorhair == 'R' || colorhair == 'r') && (coloreye != 'A' || coloreye != 'a')) {
+            cont1 = cont1 + 1
+        }
+    }
+    // Saída de dados
+    if (contpessoa != 0) {
+        mensagem = mensagem + `A quantidade de pessoas acima de 50 anos com peso menor que 60kg é: ${contpessoa}<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas acima de 50 anos com peso menor que 60kg<br>`
+    }
+
+    if (acmidade != 0) {
+        mensagem = mensagem + `A média das idades das pessoas com altura inferior a 1,50 metros é: ${(acmidade/contidade).toFixed(2)}<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas com altura inferior a 1,50 metros<br>`
+    }
+
+    if (conteye != 0) {
+        mensagem = mensagem + `Há ${((conteye/6)*100).toFixed(2)}% de pessoas com olhos azuis<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há pessoas de olhos azuis<br>`
+    }
+
+    if (cont1 != 0) {
+        mensagem = mensagem + `Há ${cont1} ruivo(s) que não tem olhos azuis<br>`
+    }
+    else {
+        mensagem = mensagem + `Não há ruivos que não possuem olhos azuis<br>`
+    }
+    document.getElementById('resultado').innerHTML = mensagem
+}
+
+function exe10() {
+    let acm = 0
+    let acm1 = 0
+    let mensagem = ''
+    
+    for (let i = 1; i <= 10; i++) {
+        let numero = Math.floor(Math.random() * 100 + 1)
+        let div = 0  // Inicializar div aqui
+
+        // Acumulando os números pares
+        if (numero % 2 == 0) {
+            acm = acm + numero
+        }
+
+        // Números primos
+        for (let aux = 1; aux <= numero; aux++) {
+            if (numero % aux == 0) {
+                div = div + 1
+            }
+        }
+
+        if (div == 2) { // Se tiver mais de 2 divisores, o número já não é primo 
+            acm1 = acm1 + numero
+        }
+    }
+
+    mensagem = mensagem + `A soma dos números pares é ${acm}<br>`
+    mensagem = mensagem + `A soma dos números primos é ${acm1}`
+    document.getElementById('resultado').innerHTML = mensagem
 }
