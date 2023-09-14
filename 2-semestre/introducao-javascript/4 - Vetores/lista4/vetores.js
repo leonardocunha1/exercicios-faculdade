132883faef1df8dd782d1b704473a43bc7912e86
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2023-09-13 22:15:39 -0300

    lista4-dia-13-09

---
+++ 2-semestre/introducao-javascript/4 - Vetores/lista4/vetores.js
@@ -0,0 +1,188 @@
+//Comandos vistos na aula: x.push(), x.lenght, x.indexOf()
+
+function exsala1 () {
+    let nota = []
+    let maior
+    let acmmedia = 0
+
+    //Entrada de dados
+    for (let i=1;i<=5;i++){
+        nota.push(Number(prompt('Informe uma nota: ')))
+    }
+
+    //Cálculos e operações
+
+    //Cálculo média
+    for (let i = 0; i<nota.length;i++) { //o vetor começa no zero, então o i = 0 obrigatoriamente!
+        acmmedia = acmmedia + nota[i]
+    }
+
+    //Cálculo maior número
+    for (let i = 0; i<nota.length;i++) { //o vetor começa no zero, então o i = 0 obrigatoriamente!
+        if (i == 0) {
+            maior = nota[i]
+        }
+        else {
+            if (nota[i] > maior) {
+                maior = nota[i]
+            }
+        }
+    }
+    //OUTRA MANEIRA DE CALCULAR O MAIOR NÚMERO
+    // maior = nota[0]
+    // for (let i = 0; i<nota.length;i++) {
+    //     if (nota[i] > maior) {
+    //        maior = nota[i]
+    //     }
+    // }
+
+    //Saída de dados
+    console.log(`A média é ${acmmedia/nota.length}\nA maior nota é ${maior}`)
+}
+
+function exsala2() {
+    let nota = []
+    let nome = []
+    let acmmedia = 0
+
+    //Entrada de dados
+    for (let i=1;i<=5;i++){
+        nome.push(prompt('Digite o nome do aluno ' + i + ': '))
+        nota.push(Number(prompt('Informe uma nota deste aluno: ')))
+    }
+
+    //Cálculos e operações
+
+    //Cálculo média
+    for (let i = 0; i<nota.length;i++) { //o vetor começa no zero, então o i = 0 obrigatoriamente!
+        acmmedia = acmmedia + nota[i]
+    }
+
+    let maior = nota[0]
+    let menor = nota[0]
+    for (let i = 0; i<nota.length;i++) {
+        //Cálculo maior número
+        if (nota[i] > maior) {
+           maior = nota[i]
+        }
+        //Cálculo menor número
+        if (nota[i] < menor) {
+            menor = nota[i]
+         }
+    }
+
+    //Saída de dados
+    console.log(`
+    A média é ${acmmedia/nota.length}
+    A maior nota foi do aluno ${nome[nota.indexOf(maior)]} com uma nota de ${maior}
+    A menor nota foi do aluno ${nome[nota.indexOf(menor)]} com uma nota de ${menor}`)
+}
+
+function ex1() {
+    let numero = []
+    //Obtendo e armazenando os números aleatórios no vetor
+    for (let i = 0 ; i < 6; i++) {
+        let numaleatorio = Math.floor(Math.random()*101) //Gerando números aleatórios entre 0 e 100
+        numero.push(numaleatorio) //Armazenando o número aleatório obtido em um vetor
+    }
+    //Cálculos e operações
+    let vetpar = [] //armazendando os valores pares do vetor em um novo vetor
+    let vetimpar = [] //armazendando os valores ímpares do vetor em um novo vetor
+    for (let i = 0 ; i < numero.length; i++) {
+        if (numero[i] % 2 == 0) {
+            vetpar.push(numero[i])
+        }
+        else {
+            vetimpar.push(numero[i])
+        }
+    }
+    //Saída de dados
+    console.log(`
+    Números pares: ${vetpar}
+    Quantidade de números pares: ${vetpar.length}
+    Números ímpares: ${vetimpar}
+    Quantidade de números ímpares: ${vetimpar.length}
+    `)
+}
+
+function ex2() {
+    let numero = []
+    //Obtendo e armazenando os números aleatórios no vetor
+    for (let i = 0 ; i < 7; i++) {
+        let numaleatorio = Math.floor(Math.random()*100) + 1 //Gerando números aleatórios entre 1 e 100
+        numero.push(numaleatorio) //Armazenando o número aleatório obtido em um vetor
+    }
+    //Cálculos e operações
+    let vetmult2 = []
+    let vetmult3 = []
+    let vetmult2e3 = []
+    for (let i = 0 ; i < numero.length; i++) {
+        if (numero[i] % 2 == 0) {
+            vetmult2.push(numero[i])
+        }
+        if (numero[i] % 3 == 0) {
+            vetmult3.push(numero[i])
+        }
+        if (numero[i] % 2 == 0 && numero[i] % 3 == 0) {
+            vetmult2e3.push(numero[i])
+        }
+    }
+    //Saída de dados
+    console.log(`
+    Múltiplos de 2: ${vetmult2}
+    Múltipos de 3: ${vetmult3}
+    Múltipos de 2 e 3: ${vetmult2e3}
+    `)
+}
+
+
+
+function ex4() {
+    let numero = []
+    //Obtendo e armazenando os números aleatórios no vetor
+    for (let i = 0 ; i < 7; i++) {
+        numero.push(Math.floor(Math.random()*6) + 25) //número aleatório entre 25 e 30
+    }
+    //Verificando se há valores iguais a 30 no vetor
+    let iguais30 = []
+    for (let i = 0; i < numero.length; i++) {
+        if (numero[i] == 30) {
+            iguais30.push(i)
+        }
+    }
+    if (iguais30.length > 0) {
+        console.log(`
+        A(s) posição(ões) que possui(em) valor(es) igual(is) a 30 é(são): ${iguais30}
+        `) 
+    }
+    else {
+        console.log(`Não possui valores iguais a 30 neste vetor`)
+    }  
+}
+
+function ex5() {
+    let logica = []
+    for (let i = 0 ; i < 4; i++) {
+        logica.push(Number(prompt(`Digite o número da matrícula do aluno na matéria de Lógica: `)))
+    }
+
+    let ling = []
+    for (let i = 0 ; i < 4; i++) {
+        ling.push(Number(prompt(`Digite o número da matrícula do aluno na matéria de Ling. Prog: `)))
+    }
+
+    let matigual = []
+    for (let i = 0 ; i < logica.length; i++) {
+        for (let j = 0 ; j < ling.length; j++) {
+            if (logica[i] == ling[j]) {
+                matigual.push(logica[i]) //também poderia ser ling[i], pois os valores são os mesmos nesta condição
+            }
+        }
+    }
+    if (matigual.length > 0) {
+        console.log(`O número das matrículas que aparecem nos dois vetores são: ${matigual}`)
+    }
+    else {
+        console.log(`Não há matrículas iguais nos dois vetores`)
+    }
+}
