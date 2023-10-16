30475aeec400e68b895b2748a1ff09b1ffca1451
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2023-10-16 15:01:09 -0300

    Update functions.js

--- 2-semestre/introducao-javascript/6 - Funções/functions.js
+++ 2-semestre/introducao-javascript/6 - Funções/functions.js
@@ -59,102 +59,127 @@ function bikeModularizado() {
     /*Faça um programa em JavaScript que solicite ao usuário dados de 10 bikes. Cada bike deve conter informações como marca, modelo, quadro, aro, ano e preço. Após a entrada de dados, o programa deve calcular e exibir ao usuário:
 
     1) a média de preço das bikes
-
     2) qual a bike é mais antiga, deve-se considerar empates
-
     3) quais as bikes são do marca Caloi
-
     4) quantas bikes possuem aro 29
-
     5) qual bike possui maior quadro, pode-se desconsiderar empates */
 
-    function promptBike() {
+
+    // Função para obter um valor positivo de entrada
+    function obterValorPositivo(mensagem) {
+        let valor = Number(prompt(mensagem));
+        while (valor < 0) {
+            valor = Number(prompt("Informe um valor positivo:"));
+        }
+        return valor;
+    }
+
+    // Função para criar um objeto de bicicleta
+    function criarObjetoBike(numeroBike) {
         let objetoBike = {
-            //Entrada de dados
-            marcaBike: prompt(`Informe a marca da bike ${i+1}`).toUpperCase(),
-            modeloBike: prompt(`Informe o modelo da bike ${i+1}`).toUpperCase(),
-            quadroBike: parseInt(prompt(`Informe o quadro da bike ${i+1}`)),
-            aroBike: Number(prompt(`Informe o aro da bike ${i+1}`)),
-            yearBike: Number(prompt(`Informe o ano da bike ${i+1}`)),
-            priceBike: Number(prompt(`Informe o valor da bike ${i+1}`))
-}
-
-    // let vetorBike = []
-    // for (let i = 0; i < 10; i++) {
-    //     let objetoBike = {
-    //         //Entrada de dados
-    //         marcaBike: prompt(`Informe a marca da bike ${i+1}`).toUpperCase(),
-    //         modeloBike: prompt(`Informe o modelo da bike ${i+1}`).toUpperCase(),
-    //         quadroBike: parseInt(prompt(`Informe o quadro da bike ${i+1}`)),
-    //         aroBike: Number(prompt(`Informe o aro da bike ${i+1}`)),
-    //         yearBike: Number(prompt(`Informe o ano da bike ${i+1}`)),
-    //         priceBike: Number(prompt(`Informe o valor da bike ${i+1}`))
-    // }
-
-        //Validações
-        while (objetoBike.quadroBike < 0) {
-            objetoBike.quadroBike = Number(prompt(`Informe um valor positivo para o quadro`));
-        }
-        while (objetoBike.aroBike < 0) {
-            objetoBike.aroBike = Number(prompt(`Informe um valor positivo para o aro`));
-        }
-        while (objetoBike.yearBike < 0) {
-            objetoBike.yearBike = Number(prompt(`Informe um valor positivo para o ano`));
-        }
-        while (objetoBike.priceBike < 0) {
-            objetoBike.priceBike = Number(prompt(`Informe um valor positivo para o preço`));
-        }
-        vetorBike.push(objetoBike);
-        console.log(`Bike inserida com sucesso!`);
+            marcaBike: prompt(`Informe a marca da bike ${numeroBike}`).toUpperCase(),
+            modeloBike: prompt(`Informe o modelo da bike ${numeroBike}`).toUpperCase(),
+            quadroBike: obterValorPositivo(`Informe o quadro da bike ${numeroBike}`),
+            aroBike: obterValorPositivo(`Informe o aro da bike ${numeroBike}`),
+            yearBike: obterValorPositivo(`Informe o ano da bike ${numeroBike}`),
+            priceBike: obterValorPositivo(`Informe o valor da bike ${numeroBike}`)
+        };
+        return objetoBike;
     }
 
-    //Realizando a média dos preços das bicicletas
-    let acumuladorPrice = 0;
-    for (let i = 0; i < vetorBike.length; i++) {
-        acumuladorPrice += vetorBike[i].priceBike;
+    // Função para preencher o vetor de bicicletas
+    function preencherVetorBike() {
+        let vetorBike = [];
+        for (let i = 0; i < 10; i++) {
+            let objetoBike = criarObjetoBike(i + 1);
+            vetorBike.push(objetoBike);
+            console.log(`Bike inserida com sucesso!`);
+        }
+        return vetorBike;
     }
-    let mediaPrice = acumuladorPrice / vetorBike.length;
+
+    // Chame a função para preencher o vetor de bicicletas
+    let vetorBike = preencherVetorBike();
+
+    // Função para calcular a média dos preços das bicicletas
+    function calcularMediaPreco(vetorBike) {
+        let acumuladorPrice = 0;
+        for (let i = 0; i < vetorBike.length; i++) {
+            acumuladorPrice += vetorBike[i].priceBike;
+        }
+        let mediaPrice = acumuladorPrice / vetorBike.length;
+        return mediaPrice;
+    }
+
+    // Função para encontrar as bicicletas mais antigas
+    function encontrarBicicletasMaisAntigas(vetorBike) {
+        let bicicletasMaisAntigas = [vetorBike[0]];
+        for (let i = 1; i < vetorBike.length; i++) {
+            if (vetorBike[i].yearBike < bicicletasMaisAntigas[0].yearBike) { //1990 < 2000 -> true. É o que está acontecendo neste if
+                bicicletasMaisAntigas = [vetorBike[i]];
+            } else if (vetorBike[i].yearBike == bicicletasMaisAntigas[0].yearBike) {
+                bicicletasMaisAntigas.push(vetorBike[i]);
+            }
+        }
+        return bicicletasMaisAntigas;
+    }
+
+    /*
+    // Função para encontrar as bicicletas da marca Caloi
+    function encontrarBicicletasCaloi(vetorBike) {
+        let bikesCaloi = [];
+        for (let i = 0; i < vetorBike.length; i++) {
+            if (vetorBike[i].marcaBike == "CALOI") {
+                bikesCaloi.push(vetorBike[i]);
+            }
+        }
+        return bikesCaloi;
+    }
+    FIZ UTILIZANDO O FILTER, MUITO MELHOR E MAIS LEGÍVEL!*/
+   // Função para encontrar as bicicletas da marca Caloi usando .filter()
+    function encontrarBicicletasCaloi(vetorBike) {
+        return vetorBike.filter(bike => bike.marcaBike === "CALOI");
+    }
+
+    // Função para contar as bicicletas com aro 29
+    function contarBicicletasAro29(vetorBike) {
+        let countAro29 = 0;
+        for (let i = 0; i < vetorBike.length; i++) {
+            if (vetorBike[i].aroBike === 29) {
+                countAro29++;
+            }
+        }
+        return countAro29;
+    }
+
+    // Função para encontrar a bicicleta com o maior quadro
+    function encontrarMaiorQuadro(vetorBike) {
+        let maiorQuadro = vetorBike[0];
+        for (let i = 1; i < vetorBike.length; i++) {
+            if (maiorQuadro.quadroBike < vetorBike[i].quadroBike) {
+                maiorQuadro = vetorBike[i];
+            }
+        }
+        return maiorQuadro;
+    }
+
+    // Chamar as funções e imprimir os resultados
+    let mediaPrice = calcularMediaPreco(vetorBike);
     console.log(`O preço médio das bicicletas é ${mediaPrice}`);
 
-    //Descobrindo a bike mais antiga
-    let bikeIdade = [];
-    bikeIdade[0] = vetorBike[0]; //Pegando todas as informações do objeto e não apenas o ano
-    for (let i = 1; i < vetorBike.length; i++) {
-        if (vetorBike[i].yearBike < bikeIdade[0].yearBike) {
-            bikeIdade = []; //Zerando o vetor
-            bikeIdade[0] = vetorBike[i];
-        }
-        else if (vetorBike[i].yearBike == bikeIdade[0].yearBike) {
-            bikeIdade.push(vetorBike[i]); //Mais uma bike com o mesmo ano
-        }
-    }
+    let bikeIdade = encontrarBicicletasMaisAntigas(vetorBike);
     console.log(`As bikes mais antigas são:`, bikeIdade);
-    //Explicando o que foi feito neste for: o vetor bikeIdade é zerado quando você encontra uma bicicleta mais antiga (ano menor) do que a que está armazenada nele. Isso garante que você esteja mantendo apenas as bicicletas mais antigas até aquele ponto e elimina qualquer bicicleta que seja menos antiga. Se houver um empate, todas as bicicletas empates são armazenadas em bikeIdade, garantindo que todas as bicicletas mais antigas sejam consideradas.
 
+    // Chamar a função para encontrar bicicletas da marca Caloi
+    let bikesCaloi = encontrarBicicletasCaloi(vetorBike);
+    console.log("Bicicletas da marca Caloi:", bikesCaloi);
 
-    //Descobrindo as bikes da marca Caloi
-    let bikesCaloi = [];
-    for (let i = 0; i < vetorBike.length; i++) {
-        if (vetorBike[i].marcaBike == "CALOI") {
-            bikesCaloi.push(vetorBike[i]);
-        }
-    }
-
-    //Contabilizando as bikes aro 29
-    let contBike29 = 0;
-    for (let i = 0; i < vetorBike.length; i++) {
-        if (vetorBike[i].aroBike == 29) {
-            contBike29++;
-        }
-    }
+    let countAro29 = contarBicicletasAro29(vetorBike);
     console.log(`O número de bikes com aro 29 é: ${countAro29}`);
 
-    //Descobrindo o maior quadro da bike
-    let maiorQuadro = vetorBike[0];
-    for (let i = 1; i < vetorBike.length; i++) {
-        if (maiorQuadro.quadroBike > vetorBike[i].quadroBike) {
-            maiorQuadro = vetorBike[i];
-        }
-    }
-    console.log(`A bike com maior quadro é: ${maiorQuadro}`);
+    let maiorQuadro = encontrarMaiorQuadro(vetorBike);
+    console.log(`A bike com maior quadro é:`, maiorQuadro);
 }
+
+// Chame a função principal para executar o programa
+bikeModularizado();
