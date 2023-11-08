a8993a174bb19088eba237a37bc385738b7b85a9
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2023-11-07 21:58:22 -0300

    att

---
+++ 2-semestre/introducao-javascript/8 - API/API PLACEHOLDER/script.js
@@ -0,0 +1,18 @@
+async function api() {
+    let user = document.getElementById("user").value;
+    let dados = await fetch(`https://jsonplaceholder.typicode.com/users/${user}`).then(response => response.json());
+    document.querySelector(".id").textContent = dados.id;
+    document.querySelector(".name").textContent = dados.name;
+    document.querySelector(".username").textContent = dados.username;
+    document.querySelector(".email").textContent = dados.email;
+    document.querySelector(".street").textContent = dados.address.street;
+    document.querySelector(".suite").textContent = dados.address.suite;
+    document.querySelector(".city").textContent = dados.address.city;
+    document.querySelector(".zipcode").textContent = dados.address.zipcode;
+    document.querySelector(".phone").textContent = dados.phone;
+    document.querySelector(".website").textContent = dados.website;
+    document.querySelector(".company").textContent = dados.company.name;
+
+    document.querySelector(".aux2").style.display = "block";
+}
+
