efbee078f3161ae836c39de090d654114c48d909
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2024-04-10 21:37:02 -0300

    at

--- 2024/DSM/1°Semestre/P.I Faculdade/js/burgers.js
+++ 2024/DSM/1°Semestre/P.I Faculdade/js/burgers.js
@@ -1,12 +1,12 @@
 let burgerJson = [
-    {id:1, name:'X-Burger', img:'images/burger1.png', price:15.90, description:'Pão, carne, queijo, alface, tomate, maionese'},
-    {id:2, name:'X-Salada', img:'images/burger2.png', price:17.50, description:'Pão, carne, queijo, alface, tomate, maionese, milho, ervilha'},
-    {id:3, name:'X-Bacon', img:'images/burger3.png', price:18.20, description:'Pão, carne, queijo, bacon, alface, tomate, maionese'},
-    {id:4, name:'X-Egg', img:'images/burger4.png', price:16.80, description:'Pão, carne, queijo, ovo, alface, tomate, maionese'},
-    {id:5, name:'X-Tudo', img:'images/burger5.png', price:20.30, description:'Pão, carne, queijo, bacon, ovo, alface, tomate, maionese, milho, ervilha, batata palha'},
-    {id:6, name:'X-Frango', img:'images/burger6.png', price:18.50, description:'Pão, filé de frango, queijo, alface, tomate, maionese'},
-    {id:7, name:'X-Veggie', img:'images/burger7.png', price:19.00, description:'Pão integral, hambúrguer vegano, queijo vegano, alface, tomate, cebola roxa, maionese vegana'},
-    {id:8, name:'X-Picanha', img:'images/burger8.png', price:22.50, description:'Pão, hambúrguer de picanha, queijo, bacon, alface, tomate, maionese'},
-    {id:9, name:'Diiner Tudo', img:'images/burger9.png', price:25.00, description:'Pão artesanal, carne, queijo, bacon, ovo, presunto, alface, tomate, cebola caramelizada, molho especial'},
-    {id:10, name:'Diinerníficon', img:'images/burger10.png', price:28.00, description:'Pão de brioche, hambúrguer de angus, queijo cheddar, bacon crocante, cebola crispy, alface, tomate, maionese de alho'}
+    {id:1, name:'Burger Palace', img:'images/burger1.png', price:15.90, description:'Pão, carne, queijo, alface, tomate, maionese', tipo: 'Um clássico irresistível para qualquer ocasião.'},
+    {id:2, name:'Grill & Go', img:'images/burger2.png', price:17.50, description:'Pão, carne, queijo, alface, tomate, maionese, milho, ervilha', tipo: 'Uma opção fresca e saudável para quem busca equilíbrio.'},
+    {id:3, name:'Burger Heaven', img:'images/burger3.png', price:18.20, description:'Pão, carne, queijo, bacon, alface, tomate, maionese', tipo: 'Para os apaixonados por bacon, uma explosão de sabor.'},
+    {id:4, name:'Eggcellent Burgers', img:'images/burger4.png', price:16.80, description:'Pão, carne, queijo, ovo, alface, tomate, maionese', tipo: 'Perfeito para os amantes de ovos e lanches substanciais.'},
+    {id:5, name:'All-In Burger', img:'images/burger5.png', price:20.30, description:'Pão, carne, queijo, bacon, ovo, alface, tomate, maionese, milho, ervilha, batata palha', tipo: 'Para os que querem tudo em um só lanche, uma experiência completa.'},
+    {id:6, name:'Chicken Delight', img:'images/burger6.png', price:18.50, description:'Pão, filé de frango, queijo, alface, tomate, maionese', tipo: 'Leve e saboroso, perfeito para os amantes de frango.'},
+    {id:7, name:'Veggie Kingdom', img:'images/burger7.png', price:19.00, description:'Pão integral, hambúrguer vegano, queijo vegano, alface, tomate, cebola roxa, maionese vegana', tipo: 'Uma opção deliciosa e sustentável para os vegetarianos.'},
+    {id:8, name:'Prime Burger', img:'images/burger8.png', price:22.50, description:'Pão, hambúrguer de picanha, queijo, bacon, alface, tomate, maionese', tipo: 'Para os amantes de carnes nobres, uma experiência única.'},
+    {id:9, name:'Diiner Tudo', img:'images/burger9.png', price:25.00, description:'Pão artesanal, carne, queijo, bacon, ovo, presunto, alface, tomate, cebola caramelizada, molho especial', tipo: 'Um delicioso hambúrguer para os gostos mais refinados e para os amantes de lanches artesanais.'},
+    {id:10, name:'Diinernífico', img:'images/burger10.png', price:28.00, description:'Pão de brioche, hambúrguer de angus, queijo cheddar, bacon crocante, cebola crispy, alface, tomate, maionese de alho', tipo: 'Um hambúrguer digno dos paladares mais exigentes, uma verdadeira obra-prima.'}
 ];
