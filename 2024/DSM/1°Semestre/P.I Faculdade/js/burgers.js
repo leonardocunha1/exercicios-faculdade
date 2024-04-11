09605589a0e5ef1b4063fbdfd8ab2578a9981156
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2024-04-10 21:28:19 -0300

    Create burgers.js

---
+++ 2024/DSM/1°Semestre/P.I Faculdade/js/burgers.js
@@ -0,0 +1,12 @@
+let burgerJson = [
+    {id:1, name:'X-Burger', img:'images/burger1.png', price:15.90, description:'Pão, carne, queijo, alface, tomate, maionese'},
+    {id:2, name:'X-Salada', img:'images/burger2.png', price:17.50, description:'Pão, carne, queijo, alface, tomate, maionese, milho, ervilha'},
+    {id:3, name:'X-Bacon', img:'images/burger3.png', price:18.20, description:'Pão, carne, queijo, bacon, alface, tomate, maionese'},
+    {id:4, name:'X-Egg', img:'images/burger4.png', price:16.80, description:'Pão, carne, queijo, ovo, alface, tomate, maionese'},
+    {id:5, name:'X-Tudo', img:'images/burger5.png', price:20.30, description:'Pão, carne, queijo, bacon, ovo, alface, tomate, maionese, milho, ervilha, batata palha'},
+    {id:6, name:'X-Frango', img:'images/burger6.png', price:18.50, description:'Pão, filé de frango, queijo, alface, tomate, maionese'},
+    {id:7, name:'X-Veggie', img:'images/burger7.png', price:19.00, description:'Pão integral, hambúrguer vegano, queijo vegano, alface, tomate, cebola roxa, maionese vegana'},
+    {id:8, name:'X-Picanha', img:'images/burger8.png', price:22.50, description:'Pão, hambúrguer de picanha, queijo, bacon, alface, tomate, maionese'},
+    {id:9, name:'Diiner Tudo', img:'images/burger9.png', price:25.00, description:'Pão artesanal, carne, queijo, bacon, ovo, presunto, alface, tomate, cebola caramelizada, molho especial'},
+    {id:10, name:'Diinerníficon', img:'images/burger10.png', price:28.00, description:'Pão de brioche, hambúrguer de angus, queijo cheddar, bacon crocante, cebola crispy, alface, tomate, maionese de alho'}
+];
