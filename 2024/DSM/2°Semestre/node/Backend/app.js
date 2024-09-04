fcffb8bd488e684585830c336e8aa5562c62ca95
Leonardo Cunha <139728213+leonardocunha1@users.noreply.github.com>
2024-09-03 22:04:26 -0300

    Aula node - acrescentei o guia.txt e adicionei algumas informações no app.js e criei a pasta api/views

--- 2024/DSM/2°Semestre/node/Backend/app.js
+++ 2024/DSM/2°Semestre/node/Backend/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const aluno = require('./routes/alunosRoutes');
-const index = require('./routes/indexRoutes');
+const aluno = require("./routes/alunosRoutes");
+const index = require("./routes/indexRoutes");
 
-app.use(express.json())
+app.use(express.json());
+
+app.set("views", path.join(__dirname, "./api/views"));
 
 const port = 3000;
 
-app.use('/aluno', aluno);
+app.use("/aluno", aluno);
 app.use("/", index);
 
 //servidor rodando
 app.listen(port, () => {
-    console.log(`Aplicativo Rodando na Porta ${port}`);
-})
-
+  console.log(`Aplicativo Rodando na Porta ${port}`);
+});
