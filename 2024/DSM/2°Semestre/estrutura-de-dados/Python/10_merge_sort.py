9c66151b8db993143e7d2471d5b7099a2e645c5d
Leonardo Cunha <139728213+leonardocunha1@users.noreply.github.com>
2024-09-06 20:22:22 -0300

    Aula Estrutura de dados 06-09-24 - Recursividade e Merge Sort

--- 2024/DSM/2°Semestre/estrutura-de-dados/Python/10_merge_sort.py
+++ 2024/DSM/2°Semestre/estrutura-de-dados/Python/10_merge_sort.py
@@ -1,8 +1,13 @@
+# variantes de estatística
+divisao = juncs = comps = 0
+
 def merge_sort(lista):
     """
     ALGORITMO DE ORDENAÇÃO MERGE SORT
     No processo de ordenação, este algoritmo 'demonta' a lista original, contendo N elementos, até obter N listas com apenas um elemento cada uma. Em seguida, usando a técnica de mesclagem (merging), "remonta" a lita, desta vez, com os elementos ordenados.
     """
+    global divisao, juncs, comps
+
     # PARTE 1: DIVISÃO DA LISTA ORIGINAL EM LISTAS MENORES
 
     # Para que possa haver divisão da lista, é necessário que ela tenha mais de um elemento. Caso contrário, a função retorna a própria lista
@@ -16,6 +21,7 @@ def merge_sort(lista):
     lista_esq = lista[:meio]
     # Tira uma cópia da segunda metade da lista
     lista_dir = lista[meio:]
+    divisao += 1
 
     # Chama a função merge_sort() recursivamente para dividir as listas em partes menores
     lista_esq = merge_sort(lista_esq)
@@ -31,6 +37,7 @@ def merge_sort(lista):
 
     # Enquanto houver elementos nas listas a serem mescladas (lista_esq e lista_dir)
     while pos_esq < len(lista_esq) and pos_dir < len(lista_dir):
+        comps += 1
         # O menor elemento é da lista da esquerda
         if lista_esq[pos_esq] < lista_dir[pos_dir]:
             # copia o elemento da lista da esquerda para a lista ordenada
@@ -55,6 +62,7 @@ def merge_sort(lista):
         sobra = lista_dir[pos_dir:]
 
     # O resultado final é a junção (concatenação) da lista ordenada com os elementos que sobraram
+    juncs += 1    
     return lista_ord + sobra
 
 ############################################################################
@@ -62,5 +70,24 @@ def merge_sort(lista):
 # lista de números desordenados
 nums = [9, 3, 6, 1, 8, 2, 4, 7, 5]
 
+
+divs = juncs = comps = 0
 # chamada da função merge_sort() passando a lista de números desordenados como argumento
+print("Antes:", nums)
 print(merge_sort(nums))
+print("Depois:", nums)
+print(f"Divisões: {divisao}, Junções: {juncs}, Comparações: {comps}")
+
+# testando com os nomes
+from data.nomes_completos import nomes
+from time import time
+import sys
+sys.dont_write_bytecode = True # não criar arquivos .pyc, ou seja, o cache
+
+# recortando a lista de nomes para testes
+nomes_teste = nomes[:10000]
+
+hora_ini = time()
+print(merge_sort(nomes_teste))
+hora_fim = time()
+print(hora_fim - hora_ini)
