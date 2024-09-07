4d2b59d010789d013e1d4f009a1ad115e90b1a1c
Leonardo Cunha <139728213+leonardocunha1@users.noreply.github.com>
2024-09-06 22:07:59 -0300

    Aula Java 06-09-24

---
+++ 2024/DSM/2°Semestre/poo-java/aulaJava01/src/main/java/Contas/TodasContas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Contas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1091392413016
+ */
+public class TodasContas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        Conta1 primeiroObj = new Conta1();
+        
+        System.out.println("Digite o nome do primeiro objeto: ");
+        String nome = input.nextLine();
+        primeiroObj.setName(nome);
+        
+        Conta2 segundoObj = new Conta2("Gomes");
+        Conta2 terceiroObj = new Conta2("Leonardo");
+        
+        System.out.printf("Os nomes de todos os objs: %s %s %s", primeiroObj.getName(), segundoObj.getName(), terceiroObj.getName());
+    }
+}
