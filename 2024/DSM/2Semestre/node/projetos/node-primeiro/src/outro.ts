a4b4b19bb4357b353f97642abd03ecbbc833b174
Leonardo Cunha <139728213+leonardocunha1@users.noreply.github.com>
2024-10-17 20:08:04 -0300

    Aula node da B7WEB

---
+++ 2024/DSM/2Semestre/node/projetos/node-primeiro/src/outro.ts
@@ -0,0 +1,4 @@
+const versao: number = 1.0;
+
+console.log(versao);
+console.log("dadasdaer3r34sdsada");
---
+++ 2024/DSM/2Semestre/node/projetos/node-primeiro/src/teste.ts
@@ -0,0 +1,6 @@
+let nome: string = 'João';
+let idade: number = 27;
+
+console.log(nome, idade);
+
+// npx ts-node teste.ts
